8e9f2485c3f91872c33aefa616373590295b768f
Thomas Rayden <thomasmwrayden@gmail.com>
2018-11-17 20:50:35 +0800

    - make sure user has enough tokens - minus spent tokens from user

--- src/api/v1/bets/bet.controller.ts
+++ src/api/v1/bets/bet.controller.ts
@@ -1,4 +1,4 @@
-import { ApiSuccess } from 'express-mate';
+import { ApiSuccess, ApiError } from 'express-mate';
 import { Response, Request } from 'express';
 import Bet from './bet.model';
 
@@ -7,6 +7,24 @@ export const createBet = async (req: Request, res: Response) => {
   const bet = new Bet({ ...body, user });
   bet.amount = 100;
   bet.chance = await Bet.chance(bet.draw._id);
+
+  /**
+   * Validate
+   */
+  if (user.tokens < bet.amount) {
+    throw new ApiError(res, 'You do not have enough tokens.');
+  }
+
+  /**
+   * Save new Bet
+   */
   await bet.save();
+
+  /**
+   * Update users tokens
+   */
+  user.tokens -= bet.amount;
+  await user.save();
+
   return new ApiSuccess(res, bet);
 };
