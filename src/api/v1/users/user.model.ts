49eba343497c7bd9261560c9e70c9ecd4d5e13f0
Thomas Rayden <thomasmwrayden@gmail.com>
2018-11-17 15:49:40 +0800

    fix: user checkPassword

--- src/api/v1/users/user.model.ts
+++ src/api/v1/users/user.model.ts
@@ -4,54 +4,78 @@ import { IUser, IUserModel } from './user.types';
 
 const RESERVED_WORDS: string[] = ['account'];
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      select: false,
+      type: String,
+      required: true
+    },
+    tokens: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0
+    }
   },
-  password: {
-    type: String,
-    required: true
-  },
-  tokens: {
-    type: Number,
-    required: true,
-    default: 0,
-    min: 0
+  {
+    toJSON: {
+      getters: true
+    },
+    toObject: {
+      getters: true
+    }
   }
-}, {
-  toJSON: {
-    getters: true
-  },
-  toObject: {
-    getters: true
-  }
-});
+);
 
-UserSchema.pre('save', async function (this: IUser) {
+UserSchema.pre('save', async function(this: IUser) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 10);
   }
 });
 
-UserSchema.path('username').validate(async function (this: IUser, username: string) {
+UserSchema.path('username').validate(async function(
+  this: IUser,
+  username: string
+) {
   const match = await this.model('User').find({ username });
   if (match.length) {
     return false;
   }
   return true;
-}, 'Username is already taken.');
+},
+'Username is already taken.');
 
-UserSchema.path('username').validate(async function(this: IUser, username: string) {
+UserSchema.path('username').validate(async function(
+  this: IUser,
+  username: string
+) {
   if (RESERVED_WORDS.includes(username)) {
     return false;
   }
   return true;
-}, 'Username is a reserved word.');
+},
+'Username is a reserved word.');
 
-UserSchema.statics.checkPassword = function (this: IUserModel, userId: string, password: string) {
-  return this.findById(userId).select('+password').exec((user: IUser) => bcrypt.compare(user.password, password));
-}
+UserSchema.statics.checkPassword = function(
+  this: IUserModel,
+  userId: string,
+  password: string
+) {
+  return this.findById(userId)
+    .select('+password')
+    .exec()
+    .then((user) => {
+      if (user) {
+        return bcrypt.compare(password, user.password);
+      }
+      return false;
+    });
+};
 
 export const User = model<IUser, IUserModel>('User', UserSchema);
 
