4dd55c34a732225a28552ba13fd9d081bf13f69a
Thomas Rayden <thomasmwrayden@gmail.com>
2018-11-17 19:40:58 +0800

    handle cron errors

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,20 +1,21 @@
 import schedule from 'node-schedule';
 import { checkFinishedDraws } from '../api/v1/draws/draw.func';
 import { giftUsersTokens } from '../api/v1/users/user.func';
+import winston = require('winston');
 
 const scheduleCronJobs = () => {
   /**
    * Run every minute
    */
   schedule.scheduleJob('*/1 * * * *', () => {
-    checkFinishedDraws().then();
+    checkFinishedDraws().then().catch((err) => { winston.error(err.message); });
   });
 
   /**
    * Run at the end of each day
    */
   schedule.scheduleJob('0 23 * * *', () => {
-    giftUsersTokens().then();
+    giftUsersTokens().then().catch((err) => { winston.error(err.message); });
   });
 };
 
