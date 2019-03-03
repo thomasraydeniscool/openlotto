2f0c1921861151d3b3fc5fc0a4468d4fea7f09f0
Thomas Rayden <thomasmwrayden@gmail.com>
2019-03-03 14:26:25 +0800

    Change DATABASE default to localhost database

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const env = envalid.cleanEnv(process.env, {
   DEVELOPMENT: bool({ default: true }),
   PORT: num({ default: 3000 }),
-  DATABASE: str({ default: 'mongodb+srv://api:CNTpC7YplsM3Dspk@email-gatherer-sandbox-xavet.gcp.mongodb.net/test?retryWrites=true' }),
+  DATABASE: str({ default: 'mongodb://localhost:27017/openlotto' }),
   TOKEN_SECRET: str({ default: 'openlotto-dev' }),
   TOKEN_EXPIRATION: num({ default: 86400000 })
 });
